7641c0527f83532c3867e317673d65b8de1c464f
esdalee <es.da.lee@gmail.com>
2019-06-08 11:49:37 -0400

    dynamically generate list of ingredients

--- logic.js
+++ logic.js
@@ -1,3 +1,4 @@
+// Recipe Search Page
 
 // Example of an API query for ingredients chicken, mushrooms, garlic
 // https://api.edamam.com/search?q=chicken,garlic,mushrooms&app_id=a2545d79&app_key=f43e58c104b981cd9a7ef77393c1cbad
@@ -9,18 +10,44 @@ var appId = "a2545d79";
 var ingrSearch = "";
 
 // Click handler for ingredients submit button
-
 $(".btn").on("click", function(e) {
   e.preventDefault();
+
+  // Clear out previous list of ingredients
+  $("#fridgeIngredients").empty();
 
   // Grab list of ingredients from user input
   ingrSearch = $("#ingredientList").val().trim();
   console.log(ingrSearch);
 
+  // Clear out text box
+  $("#ingredientList").empty();
+
+  // List out searched ingredients
+  // Save string of all ingredients in an array
+  var ingrArray = ingrSearch.split(',');
+  console.log(ingrArray);
+
+  // Create div and list out every ingredient
+  for (i=0; i<ingrArray.length; i++) {
+    var ingrDiv = $('<div/>', {
+      text: ingrArray[i],
+      id: 'ingrDiv'+i
+    });
+    // Create span to delete
+    var ingrSpan = $('<span/>', {
+      text: 'x',
+      id: 'deleteIngr'
+    });
+    // Append span to div
+    ingrDiv.append(ingrSpan);
+    $("#fridgeIngredients").append(ingrDiv);
+  };
+
   // Construct new query string with user inputs
   var newURL = baseQuery + ingrSearch + "&app_id=" + appId + "&app_key=" + apiKey;
 
-  // Ajax call
+  // Ajax call to Edamam API to grab recipes
   $.ajax({
       url: newURL,
       method: "GET"
@@ -60,3 +87,14 @@ $(".btn").on("click", function(e) {
   });
 
 });
+
+// Click handler for removing ingredient(s)
+$(document).on("click", "#deleteIngr", function(e) {
+  e.preventDefault();
+  console.log(this);
+  $(this).closest("div").remove();
+});
+
+// TO DO: 
+// Make exception cases for if user puts in , at the end of ingredient list
+// Duplicated ingredients
